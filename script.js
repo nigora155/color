afa5b9749cc367374cf5c7289fd65e952da9aa4e
nigora155 <nigoradjumashevs@gmail.com>
2025-04-23 13:00:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let text1 = document.querySelector(".text1");
+let body1 = document.querySelector("body");
+
+let yoz = prompt('Введите цвет текста ');
+let yoz1 = prompt('Введите цвет фона страницы');
+
+text1.style.color = yoz;
+body1.style.backgroundColor = yoz1;
